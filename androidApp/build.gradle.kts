0cdbd5e27c3947497662a37cf8b4d724a48227f7
cora32 <cora32@tuta.io>
2026-08-19 17:10:16 +0300

    [CI] Android build fix.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -63,6 +63,7 @@ android {
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
